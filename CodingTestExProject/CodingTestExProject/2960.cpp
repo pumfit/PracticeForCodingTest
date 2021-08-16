c768c26b77b25288a4647837bd1e1479189ae43b
joo98 <slew61@naver.com>
2021-08-16 11:35:42 +0900

    2960 풀이

--- CodingTestExProject/CodingTestExProject/2960.cpp
+++ CodingTestExProject/CodingTestExProject/2960.cpp
@@ -4,6 +4,11 @@
 N보다 작거나 같은 모든 소수를 찾는 알고리즘
 에서 K번째로 지우는 수가 무엇인지 찾는 문제
 
+210816 재도전
+주어진 문제의 순서에 맞게 구성하고 나서 k번째에 대해 찾아나갔다.
+while문을 두번쓰다보니 첫 반복문에 break조건을 안 넣어서 답은 나오는데 오류가 나기도했다.
+다른 풀이를 살펴보니 중첩 for문으로 깔끔하게 푸는 풀이들도 있었다. 다른 풀이들도 보고 어떻게
+단순하게 풀 수 있는지 살펴보아야겠다.
 */
 
 #include <iostream>
@@ -11,82 +16,39 @@ using namespace std;
 
 int main()
 {
-	int N, K;
-	int Idx= 0;
+	int N,K;
+	int idx = 0;
 	bool arr[1001] = { 0, };
 
 	cin >> N >> K;
 
-	for (int i = 2; i < N; i++)
+	int p = 2;
+	while (true)
 	{
-		if (arr[i] == 0)
+		int temp = p;
+		while (temp <= N)
 		{
-
-			for (int j = i; j < N; j += i)
+			if (!arr[temp])
 			{
-				Idx++;
-
-				if (arr[j] == 0)
+				arr[temp] = true;//지움
+				idx++;
+				if (idx == K)
 				{
-					arr[j] = 1;
-				}
-				if (Idx == K)
-				{
-					cout << j;
+					cout << temp;
 					break;
 				}
 			}
-
+			temp += p;
 		}
-		if (Idx == K)
+		while (true)
+		{
+			p++;
+			if (!arr[p])
+				break;
+		}
+		if (idx == K)
 		{
 			break;
 		}
-		
 	}
-
 }
-/*
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int N, K;
-	int Idx= 0;
-	bool arr[1001] = { 0, };
-
-	cin >> N >> K;
-
-	for (int i = 2; i < N; i++)
-	{
-		if (arr[i] == 0)
-		{
-
-			for (int j = i; j < N; j += i)
-			{
-				Idx++;
-
-				if (arr[j] == 0)
-				{
-					arr[j] = 1;
-				}
-				if (Idx == K)
-				{
-					cout << j;
-					break;
-				}
-			}
-
-		}
-		if (Idx == K)
-		{
-			break;
-		}
-
-	}
-
-}
-
-*/
