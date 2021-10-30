00400b092572746c11aa2f90aee247f4f90ee535
joo98 <slew61@naver.com>
2021-10-31 00:54:25 +0900

    1389 풀이

--- CodingTestExProject/CodingTestExProject/1389.cpp
+++ CodingTestExProject/CodingTestExProject/1389.cpp
@@ -2,6 +2,10 @@
 1389 케빈 베이컨의 6단계 법칙
 
 vector subscript out of range
+
+진짜 약간 이상해지면 아무것도 안보이나보다..
+29번 라인에서 두번째 조건식 i< 없이 graph[nx].size()만 써서 vector subscript out of range 나고
+왜지?? 뭐가 문제지 삽질 하루동안 했음 진짜.....역대급...정신차리자
 */
 #include <iostream>
 #include <vector>
@@ -22,7 +26,7 @@ void bfs(int x)
 	{
 		int nx = q.front();
 		q.pop();
-		for (int i = 0; graph[nx].size(); i++)
+		for (int i = 0; i < graph[nx].size(); i++)
 		{
 			int y = graph[nx][i];
 			if (visited[y] == 0)
@@ -43,23 +47,26 @@ int main() {
 		graph[x].push_back(y);
 		graph[y].push_back(x);
 	}
-	int min = 0, idx = 1;
 
-	for (int i = 1; i < N; i++)
+	int m = 999, idx = 1;
+
+	for (int i = 1; i <= N; i++)
 	{
 		int sum = 0;
 		bfs(i);
-		for (int j = 0; j < N; j++)
+		for (int j = 1; j <= N; j++)
 		{
 			if (i != j)
-				sum += visited[j];
+			{
+				sum += visited[j]-1;
+			}
 		}
-		memset(visited, 0, sizeof(visited));
-		if (sum < min)
+		if (sum < m)
 		{
-			sum = min;
+			m = sum;
 			idx = i;
 		}
+		memset(visited, 0, sizeof(visited));
 	}
 	cout << idx;
 	return 0;
