cb95a719860fd937445b209cec958bbb53247edc
joo98 <slew61@naver.com>
2022-01-02 19:20:18 +0900

    2798 풀이

---
+++ CodingTestExProject/CodingTestExProject/2798.cpp
@@ -0,0 +1,89 @@
+/*
+2798 블랙잭
+
+카지노에서 제일 인기 있는 게임 블랙잭의 규칙은 상당히 쉽다. 카드의 합이 21을 넘지 않는 한도 내에서, 카드의 합을 최대한 크게 만드는 게임이다. 블랙잭은 카지노마다 다양한 규정이 있다.
+한국 최고의 블랙잭 고수 김정인은 새로운 블랙잭 규칙을 만들어 상근, 창영이와 게임하려고 한다.
+
+김정인 버전의 블랙잭에서 각 카드에는 양의 정수가 쓰여 있다. 그 다음, 딜러는 N장의 카드를 모두 숫자가 보이도록 바닥에 놓는다. 그런 후에 딜러는 숫자 M을 크게 외친다.
+이제 플레이어는 제한된 시간 안에 N장의 카드 중에서 3장의 카드를 골라야 한다. 블랙잭 변형 게임이기 때문에, 플레이어가 고른 카드의 합은 M을 넘지 않으면서 M과 최대한 가깝게 만들어야 한다.
+N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력하시오.
+
+bf로 3중 for문으로 풀수 있다는 것 까진 알겠고 해당 풀이가 맞긴한데
+각 for문 마다 변수를 선언하고 따로 조건없이 진행해 풀이 이후 어떻게 하면 효율적으로 조건을 넣어줄 수 있을까?? 라는 생각이 들었다.
+
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, M;
+	int ans = 0;
+	int arr[101];
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < N; i++)
+	{
+		int sum = 0;
+		sum += arr[i];
+		for (int j = 0; j < N; j++)
+		{
+			if (i != j)
+			{
+				int ssum = sum;
+				ssum += arr[j];
+				for (int k = 0; k < N; k++)
+				{
+					if (k != i && k != j)
+					{
+						int sssum = ssum;
+						sssum += arr[k];
+						if (sssum <= M)
+							ans = max(sssum, ans);
+					}
+				}
+			}
+		}
+
+	}
+	cout << ans;
+	return 0;
+}
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N,M;
+	int ans = 0;
+	int arr[101];
+
+	cin>>N>>M;
+	for(int i=0;i<N;i++)
+		cin>>arr[i];
+
+	for(int i=0;i<N-2;i++)
+	{
+		for(int j=i+1;j<N-1;j++)
+		{
+			if(i!=j)
+			{
+				for(int k=j+1;k<N;k++)
+				{
+					if(k != i&&k != j)
+					{
+						int sum = arr[i]+arr[j]+arr[k];
+						if(sum<=M)
+							ans = max(ans,sum);
+					}
+				}
+			}
+		}
+
+	}
+	cout<<ans;
+	return 0;
+}
+*/
