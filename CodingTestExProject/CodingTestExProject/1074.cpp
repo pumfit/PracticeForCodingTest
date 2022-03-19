81d5582c6c488c224ea6599d8be11ae6796f5519
joo98 <slew61@naver.com>
2022-03-19 21:26:10 +0900

    1074 풀이

---
+++ CodingTestExProject/CodingTestExProject/1074.cpp
@@ -0,0 +1,32 @@
+/*
+1074 Z
+
+한수는 크기가 2N × 2N인 2차원 배열을 Z모양으로 탐색하려고 한다. 예를 들어, 2×2배열을 왼쪽 위칸, 오른쪽 위칸, 왼쪽 아래칸, 오른쪽 아래칸 순서대로 방문하면 Z모양이다.
+
+N > 1인 경우, 배열을 크기가 2N-1 × 2N-1로 4등분 한 후에 재귀적으로 순서대로 방문한다.
+
+N이 주어졌을 때, r행 c열을 몇 번째로 방문하는지 출력하는 프로그램을 작성하시오.
+
+https://www.acmicpc.net/problem/1074
+
+*/
+#include <iostream>
+using namespace std;
+
+int func(int n, int r, int c) {
+	if (n == 0) return 0;
+	int half = 1 << (n - 1);
+	if (r < half && c < half)//1번구역
+		return func(n - 1, r, c);
+	if (r < half && c >= half)//2번구역
+		return half * half + func(n - 1, r, c - half);
+	if (r >= half && c < half)//3번구역
+		return 2 * half*half + func(n - 1, r - half, c);
+	return 3 * half*half + func(n - 1, r - half, c - half);//4번구역
+}
+
+int main(void) {
+	int n, r, c;
+	cin >> n >> r >> c;
+	cout << func(n, r, c);
+}
