604091cf1f95b85f9052b3a325aca720f72aa00f
joo98 <slew61@naver.com>
2022-04-20 22:18:31 +0900

    1920 이분탐색 풀이

--- CodingTestExProject/CodingTestExProject/1920.cpp
+++ CodingTestExProject/CodingTestExProject/1920.cpp
@@ -34,3 +34,55 @@ int main() {
 	}
 	return 0;
 }
+/*
+구현시 헷갈렸던 부분
+start <= end 조건을 start < end로 처음에 적었었다.
+- 시작점이 끝보다 작거나 같아야한다. (이유:start == end인 경우는 target이 범위의 중앙인 경우이다.)
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M, n, m;
+int arr[100001] = { 0, };
+
+int BinarnySearch(int target)
+{
+	int start = 0;
+	int end = N - 1;
+	while (start <= end)
+	{
+		int mid = (start + end) / 2;
+		if (arr[mid] > target)
+		{
+			end = mid - 1;
+		}
+		else if (arr[mid] < target)
+		{
+			start = mid + 1;
+		}
+		else {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	cin >> M;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> m;
+		cout << BinarnySearch(m) << '\n';
+	}
+	return 0;
+}
