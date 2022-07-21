b1bd1d83790f22bad6c9c042fe72bee40e95d1c0
joo98 <slew61@naver.com>
2022-07-21 21:33:54 +0900

    올바른 괄호 풀이

---
+++ CodingTestExProject/CodingTestExProject/프로그래머스/CorrectParenthesis.cpp
@@ -0,0 +1,35 @@
+/*
+¿Ã¹Ù¸¥ °ýÈ£
+https://school.programmers.co.kr/learn/courses/30/lessons/12909
+*/
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<int> correct;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            correct.push(1);
+        }
+        else
+        {
+            if (correct.empty())
+            {
+                return false;
+            }
+            else {
+                correct.pop();
+            }
+        }
+    }
+    if (!correct.empty())
+        return false;
+    return true;
+}
